d05c737cc283f117b0518bc14a70c68f65cf116e
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-12 18:09:30 +0100

    Index page from jinja2 template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello from Flask!</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
