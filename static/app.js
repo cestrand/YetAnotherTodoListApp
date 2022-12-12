49cadf94724136cf3b9c16c3c3722fc41a8caa7c
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-12 19:09:55 +0100

    adding tasks to form with jQuery

--- static/app.js
+++ static/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
     console.log("hi");
+
+    TodoForm.install();
 });
+
+const TodoForm = {
+    add: function(text) {
+        $("#todo-list ul").append(`
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+        ${text}
+        <a href="#"><span class="badge bg-secondary rounded-pill">Delete</span></a>
+        </li>
+        `)
+    },
+    todoInput: function() {
+        return $("#todo-list div input[type=text]");
+    },
+    install: function() {
+        $("#todo-list #button-add").on("click", function(e) {
+            let $todoInput = TodoForm.todoInput();
+            let text = $todoInput.val();
+            TodoForm.add(text);
+        })
+    }
+
+}
