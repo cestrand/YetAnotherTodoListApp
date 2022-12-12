c335ca68583c7835992ffa98ccb9656908c6fca4
Marcin Kolenda <marcinkolenda419@gmail.com>
2022-12-12 17:53:35 +0100

    local run config

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<p>Hello from Flask!</p>"
+
+if __name__ == "__main__":
+    app.run(
+        debug = os.environ("DEBUG", False)
+    )
